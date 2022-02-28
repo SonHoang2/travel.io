50c98555542b262006d276061bbe5ec486e31f3e
sonhoang <doremon.dm43@gmail.com>
2022-02-28 20:01:42 +0700

    8:00pm  28/2/2022

--- assets/js/carousel-slide.js
+++ assets/js/carousel-slide.js
@@ -1,4 +1,8 @@
 const carouselSlideImg = document.querySelector('.carousel-slide__img')
+const carouselSlideTextH5 = document.querySelector('.carousel-slide-text__h5')
+const carouselSlideTextH1 = document.querySelector('.carousel-slide-text__h1')
+const carouselSlideTextp = document.querySelector('.carousel-slide-text__p')
+
 let images = [
     'assets/img/img change/skiing-slider-img-1.jpg',
     'assets/img/img change/skiing-slider-img-2.jpg'
@@ -12,29 +16,47 @@ let step = 0;
 function slideit(){
     carouselSlideImg.src = images[step]
     if (step < images.length - 1 ) {
+        carouselSlideTextH5.innerText = 'From alps'
+        carouselSlideTextH1.innerText = 'Snow Adventure'
         step++
     }
-    else step = 0
+    else {
+        carouselSlideTextH5.innerText = 'Enjoy Your'
+        carouselSlideTextH1.innerText = 'Winter Vacations'
+        step = 0
+    }
 }
 
 preBtn.addEventListener('click', function() {
     if (step > images.length - 1 ) {
         step = 0
         carouselSlideImg.src = images[step]
+        carouselSlideTextH5.innerText = 'Enjoy Your'
+        carouselSlideTextH1.innerText = 'Winter Vacations'
     }
-    else carouselSlideImg.src = images[step++]
+    else {
+        carouselSlideImg.src = images[step++]
+        carouselSlideTextH5.innerText = 'From alps'
+        carouselSlideTextH1.innerText = 'Snow Adventure'
+    }
 })
 
 nextBtn.addEventListener('click', function() {
     if (step > images.length - 1 ) {
         step = 0
         carouselSlideImg.src = images[step]
+        carouselSlideTextH5.innerText = 'Enjoy Your'
+        carouselSlideTextH1.innerText = 'Winter Vacations'
     }
-    else carouselSlideImg.src = images[step++]
+    else {
+        carouselSlideImg.src = images[step++]
+        carouselSlideTextH5.innerText = 'From alps'
+        carouselSlideTextH1.innerText = 'Snow Adventure'
+    }
 })
 
 slideit()
-window.onload = setInterval(slideit, 3000)
+window.onload = setInterval(slideit, 5000)
 
 
 
