5107f38b900e2e1fbccb3b109071dca32173db7e
sonhoang <doremon.dm43@gmail.com>
2022-03-05 19:09:58 +0700

    3/5/2022

--- assets/js/carousel-slide.js
+++ assets/js/carousel-slide.js
@@ -1,69 +1,69 @@
-const carouselSlideImg = document.querySelector('.carousel-slide__img')
-const carouselSlideText = document.querySelectorAll('.carousel-slide-text')
+const carouselSlideImg = document.querySelector('.carousel-slide__img');
+const carouselSlideText = document.querySelectorAll('.carousel-slide-text');
 
 let images = [
     'assets/img/img change/skiing-slider-img-1.jpg',
     'assets/img/img change/skiing-slider-img-2.jpg'
-]
+];
 // button
-const preBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
+const preBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
 // counter 
 let step = 0;
 function slideit(){
-    carouselSlideImg.src = images[step]
+    carouselSlideImg.src = images[step];
     if (images[step] != undefined) {
         if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear')
-            step++
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
+            step++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear')
-            step = 0
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
+            step = 0;
         }
     }
-    else step = 0
+    else step = 0;
 }
 
 preBtn.addEventListener('click', function() {
-    carouselSlideImg.src = images[step]
+    carouselSlideImg.src = images[step];
     if (images[step] != undefined) {
         if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear')
-            step++
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
+            step++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear')
-            step = 0
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
+            step = 0;
         }
     }
-    else step = 0
+    else step = 0;
 })
 
 nextBtn.addEventListener('click', function() {
-    carouselSlideImg.src = images[step]
+    carouselSlideImg.src = images[step];
     if (images[step] != undefined) {
         if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear')
-            step++
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
+            step++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear')
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear')
+            carouselSlideText[step].classList.add('carousel-slide-text--appear');
+            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
             step = 0
         }
     }
-    else step = 0
+    else step = 0;
 })
 
-slideit()
-window.onload = setInterval(slideit, 5000)
+slideit();
+window.onload = setInterval(slideit, 5000);
 
 
 
