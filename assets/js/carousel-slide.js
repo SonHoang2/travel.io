10326c905298d837ac2377e759dac27499e8f022
sonhoang <doremon.dm43@gmail.com>
2022-03-25 21:47:36 +0700

    25/3/2022

--- assets/js/carousel-slide.js
+++ assets/js/carousel-slide.js
@@ -1,60 +1,61 @@
 const carouselSlideImg = document.querySelector('.carousel-slide__img');
 const carouselSlideText = document.querySelectorAll('.carousel-slide-text');
+
 // button
 const preBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
 // counter 
-let step = 0;
+let i = 0;
 function slideit(){
-    carouselSlideImg.src = images[step];
-    if (images[step] != undefined) {
-        if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
-            step++;
+    carouselSlideImg.src = carouselSlideImgages[i];
+    if (carouselSlideImgages[i] != undefined) {
+        if (i < carouselSlideImgages.length - 1 ) {
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i + 1].classList.remove('carousel-slide-text--appear');
+            i++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
-            step = 0;
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i - 1].classList.remove('carousel-slide-text--appear');
+            i = 0;
         }
     }
-    else step = 0;
+    else i = 0;
 }
 
 preBtn.addEventListener('click', function() {
-    carouselSlideImg.src = images[step];
-    if (images[step] != undefined) {
-        if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
-            step++;
+    carouselSlideImg.src = carouselSlideImgages[i];
+    if (carouselSlideImgages[i] != undefined) {
+        if (i < carouselSlideImgages.length - 1 ) {
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i + 1].classList.remove('carousel-slide-text--appear');
+            i++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
-            step = 0;
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i - 1].classList.remove('carousel-slide-text--appear');
+            i = 0;
         }
     }
-    else step = 0;
+    else i = 0;
 })
 
 nextBtn.addEventListener('click', function() {
-    carouselSlideImg.src = images[step];
-    if (images[step] != undefined) {
-        if (step < images.length - 1 ) {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step + 1].classList.remove('carousel-slide-text--appear');
-            step++;
+    carouselSlideImg.src = carouselSlideImgages[i];
+    if (carouselSlideImgages[i] != undefined) {
+        if (i < carouselSlideImgages.length - 1 ) {
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i + 1].classList.remove('carousel-slide-text--appear');
+            i++;
         }
         else {
-            carouselSlideText[step].classList.add('carousel-slide-text--appear');
-            carouselSlideText[step - 1].classList.remove('carousel-slide-text--appear');
-            step = 0
+            carouselSlideText[i].classList.add('carousel-slide-text--appear');
+            carouselSlideText[i - 1].classList.remove('carousel-slide-text--appear');
+            i = 0
         }
     }
-    else step = 0;
+    else i = 0;
 })
 
 slideit();
